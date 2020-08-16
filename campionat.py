3a28c1abf6be18a3875e0c34b79d2b1ac0e2bd1b
Dorbe16 <57238116+Dorbe16@users.noreply.github.com>
2020-08-17 02:31:35 +0300

    Update campionat.py

--- campionat.py
+++ campionat.py
@@ -1,4 +1,3 @@
-#CAMPIONAT
 def key(elem):
     return elem[1]
 k = int(input())
@@ -13,24 +12,16 @@ for i in range(n):
     scor1 = int(meci[1])
     scor2 = int(meci[3])
     echipa2 = meci[4]
+    if echipa1 not in punctaje.keys():
+        punctaje.update({echipa1: 0})
+    if echipa2 not in punctaje.keys():
+        punctaje.update({echipa2: 0})
     if scor1 == scor2:
-        if echipa1 not in punctaje.keys():
-            punctaje.update({echipa1: 1})
-        else:
-            punctaje.update({echipa1: punctaje[echipa1] + 1})
-        if echipa2 not in punctaje.keys():
-            punctaje.update({echipa2: 1})
-        else:
-            punctaje.update({echipa2: punctaje[echipa2] + 1})
+        punctaje.update({echipa1: punctaje[echipa1] + 1})
+        punctaje.update({echipa2: punctaje[echipa2] + 1})
     elif scor1 > scor2:
-        if echipa1 not in punctaje.keys():
-            punctaje.update({echipa1: 3})
-        else:
             punctaje.update({echipa1: punctaje[echipa1] + 3})
     elif scor2 > scor1:
-        if echipa2 not in punctaje.keys():
-            punctaje.update({echipa2: 3})
-        else:
             punctaje.update({echipa2: punctaje[echipa2] + 3})
     if echipa1 not in goluriDate.keys():
         goluriDate.update({echipa1: scor1})
@@ -42,12 +33,13 @@ for i in range(n):
         goluriDate.update({echipa2: goluriDate[echipa2] + scor2})
     if echipa1 not in goluriPrimite.keys():
         goluriPrimite.update({echipa1: scor2})
-    else: 
+    else:
         goluriPrimite.update({echipa1: goluriPrimite[echipa1] + scor2})
     if echipa2 not in goluriPrimite.keys():
         goluriPrimite.update({echipa2: scor1})
-    else: 
+    else:
         goluriPrimite.update({echipa2: goluriPrimite[echipa2] + scor1})
+
 for i in punctaje.keys():
     meciuri.append(tuple([i, punctaje[i], goluriDate[i], goluriPrimite[i]]))
 meciuri = sorted(meciuri, key = key, reverse = True)
